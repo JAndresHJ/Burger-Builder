6d26ade66be67b21d0e81029e5168d7f84372d47
Jorge Andrés Hernández <j.hernandez@cignuz.com>
2019-05-07 12:46:09 -0500

    Layout, Logo, Navigation and Responsive adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => {
-    return(
-        <>
-            <div> Toolbar, SideDrawe, Backdrop </div>
-            <main className={classes.Content}>  
-                {props.children}
-            </main>
-        </>
-    );
+class Layout extends Component {
+
+    state = {
+        showSideDrawer : false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer : false
+        })
+    }
+
+    // Setting the state when it depends on the old state
+    sideDrawerToogleHandler = () => {
+        this.setState( ( prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <Toolbar clicked={this.sideDrawerToogleHandler}/>
+                <SideDrawer 
+                    show={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>  
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
 }
 
-export default layout
+export default Layout
